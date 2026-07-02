e8b80c4d754f103d02c2d3f35be92d890572ffbf
krbyjnnn <k.palconete.559180@umindanao.edu.ph>
2026-07-02 20:27:10 +0800

    new commits

--- resources/views/dashboard-kerb.blade.php
+++ resources/views/dashboard-kerb.blade.php
@@ -41,7 +41,7 @@
                   let tokens = entry.body.match(/\n+|\S+/g) || [];
                   
                   // FIXED: Changed budget to a safe 65 words per page to stop any bottom clipping on mobile
-                  let maxWordsPerPage = 65; 
+                  let maxWordsPerPage = 100; 
                   
                   let currentChunk = [];
                   let wordsOnThisPage = 0;
